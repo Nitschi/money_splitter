2bf250a2e800d8bc9b0543c0bf67ed35b6a001fd
Laura <laura@prinzip.com>
2023-07-02 22:08:44 +0200

    feat(frontend): add configuration for backend in Docker

--- dev-backend-in-docker.ps1
+++
@@ -1,6 +0,0 @@
-docker pull mcr.microsoft.com/dotnet/sdk:7.0
-docker build ./backend -t flup_backend
-docker run -ti -p 55555:55555 --rm flup_backend
-
-Write-Host -NoNewLine 'Press any key to continue...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
