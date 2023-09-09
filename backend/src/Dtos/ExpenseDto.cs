bbffdbc7dba1d42a6b635fb2746f201e7bac716d
anitsch <Alexander.Nitsch@scs.ch>
2023-09-09 12:08:41 +0200

    fix: validation must be on constructor parameters (but also on properties)

--- backend/src/Dtos/ExpenseDto.cs
+++ backend/src/Dtos/ExpenseDto.cs
@@ -4,15 +4,6 @@ namespace money_splitter.Dtos;
 
 public record ExpenseDto
 {
-    public ExpenseDto(string Description, int Amount, DateTime Time, PersonDto PaidBy, List<PersonDto> PaidFor)
-    {
-        this.Description = Description;
-        this.Amount = Amount;
-        this.Time = Time;
-        this.PaidBy = PaidBy;
-        this.PaidFor = PaidFor;
-    }
-
     [Required] public string Description { get; init; }
     [Required] public int Amount { get; init; }
     [Required] public DateTime Time { get; init; }
--- backend/src/Dtos/PersonDto.cs
+++ backend/src/Dtos/PersonDto.cs
@@ -4,12 +4,6 @@ namespace money_splitter.Dtos;
 
 public record PersonDto
 {
-    public PersonDto(Guid Id, string Name)
-    {
-        this.Id = Id;
-        this.Name = Name;
-    }
-
     [Required] public Guid Id { get; init; }
     [Required] public string Name { get; init; }
 }
